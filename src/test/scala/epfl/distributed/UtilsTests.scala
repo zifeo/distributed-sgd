8fffae63c6d34b67f8bbf0e50d17042efed11679
Teo Stocco <teo@zifeo.com>
2018-03-19 00:44:49 +0100

    add tests

---
+++ src/test/scala/epfl/distributed/UtilsTests.scala
@@ -0,0 +1,18 @@
+package epfl.distributed
+
+class UtilsTests extends Spec {
+
+  "Utilities" when {
+
+    "being used" should {
+
+      "work" in {
+        println("yes")
+      }
+
+
+    }
+
+  }
+
+}
