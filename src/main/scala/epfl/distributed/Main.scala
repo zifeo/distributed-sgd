5cfcc6506bee9e591d1ccb91e78c03f53e89f698
Teo Stocco <teo@zifeo.com>
2018-03-19 00:58:34 +0100

    apply style

--- src/main/scala/epfl/distributed/Main.scala
+++ src/main/scala/epfl/distributed/Main.scala
@@ -6,17 +6,7 @@ import io.grpc.{ManagedChannelBuilder, ServerBuilder}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-//import Vec._
-
 object Main extends App {
-
-  val v1 = Vec(1, 2, 3)
-  val v2 = Vec(1, 2, 3)
-
-  v1 + v2
-  v1 dot v2
-  v1 * 2
-  v1.norm
 
   class GreeterImpl extends GreeterGrpc.Greeter {
     override def sayHello(req: HelloRequest) = {
@@ -29,7 +19,7 @@ object Main extends App {
   val server = ServerBuilder.forPort(config.port).addService(GreeterGrpc.bindService(new GreeterImpl, global)).build
   server.start()
 
-  // no plaintext requires some netty-bundling
+  // no plaintext requires some netty-certificate-bundling
   val channel = ManagedChannelBuilder.forAddress("127.0.0.1", config.port).usePlaintext(true).build
   val stub    = GreeterGrpc.blockingStub(channel)
 
